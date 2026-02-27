c8133cc618a34f6118f9a05333bb023dda7e8e6d
vivek <vivektaak365@gmail.com>
2026-02-27 11:26:59 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('href');
+
+        if (targetId === '#') return;
+
+        const targetSection = document.querySelector(targetId);
+        if (!targetSection) return;
+
+        e.preventDefault();
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+//product hover animations
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
+});
